54bd009611b37d716ebded7baebbe7c6449828e6
zhenhuanlee <zhenhuanlee@outlook.com>
2018-06-03 15:07:55 +0800

    struct

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 fn main() {
-    test1();
+    // test1();
+    // test2();
+    // test3();
+    // test4();
+    // test5();
+    // test6();
+    test10();
 }
 
 fn test1 () {
@@ -31,7 +37,7 @@ fn test3() {
     let s = String::from("hello");
 
     test3_1(s); // s的值转移进了方法
-    println!(s); // 非法，不可以再使用
+    // println!(s); // 非法，不可以再使用
 
     let x = 5;  // x不是地址空间，而是编译成一个固定长度  
     test3_2(x); 
@@ -44,6 +50,90 @@ fn test3_1(sth: String) {
 
 fn test3_2(si: i32) {
     println!("{}", si);
+}
+
+// 传指调用
+fn test4() {
+    let s1 = String::from("hello");
+    let len = test4_1(&s1);
+
+    println!("the length of {} is {}", s1, len);
+}
+
+fn test4_1(s: &String) -> usize {
+    s.len()
+}
+
+fn test5() {
+    let mut s = String::from("hello");
+    test5_1(&mut s);
+}
+
+fn test5_1(s: &mut String) {
+    s.push_str(", world");
+}
+
+fn test6() {
+    let mut s = String::from("hello");
+    let mut s2 = String::from("world");
+
+    let r1 = &mut s;
+    // let r10 = &s; // 只要不是可改的，就可以随便引用
+    // let r2 = &mut s; // 这里会报 second mutable borrow occurs here
+    { // 加个scope
+        let r3 = &s2;
+    }
+    let r4 = &mut &s2;
+}
+
+// string slice
+// slice 当borrow发生时，
+fn test7() {
+    let s = String::from("hello world!");
+
+    let hello = &s[0..5];
+    let world = &s[6..];
+    println!("${}$", hello);
+    println!("${}$", world);
+}
+
+fn test8() {
+    let mut s = String::from("hello world");
+    let bytes = s.as_bytes();
+    for(i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            println!("{}", &s[0..i]);
+            break;
+        }
+    }
+    println!("{}", &s[..]);
+}
+
+fn test9() {
+    let a = [1, 2, 3, 4];
+    let slice = &a[1..3];
+
+    println!("{}", slice[1]);
+}
+
+
+// strcut
+#[derive(Debug)]  // 很关键
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
+}
+fn test10() {
+    let user1 = User {
+        email: String::from("someone@mail.com"),
+        username: String::from("somename"),
+        active: true,
+        sign_in_count: 1,
+    };
+
+    println!("{:#?}", user1); // '#'format的意思
 }
 
 
